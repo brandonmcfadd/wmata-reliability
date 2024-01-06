8b92183a5c3ebc567aac5065523e58ae98e21edc
brandon <brandon@brandonmcfadden.com>
2024-01-05 20:02:38 -0600

    Update production-upgrade.sh

--- production-upgrade.sh
+++ production-upgrade.sh
@@ -4,7 +4,6 @@ python3 -m pip install --upgrade pip
 sudo git stash
 sudo git stash drop
 sudo git pull
-sudo chown -R brandon_brandonmcfadden_com:brandon_brandonmcfadden_com .
+sudo chown -R brandonmcfadden:brandonmcfadden .
 sudo chmod +x production-upgrade.sh
-pip install -r /home/brandon_brandonmcfadden_com/wmata-reliability/requirements.txt
 sudo systemctl restart wmata-reliability.service
